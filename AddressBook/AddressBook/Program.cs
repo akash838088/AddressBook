046ad2f7ac2e02735cf58b0c41fabd7b7a29010c
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-17 22:08:41 +0530

    [Akash] Add: UC2-AddNewContact

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBooks
+namespace AddressBookProgram
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Address Book problem");
+            AddressBook addBook = new AddressBook();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select 1 : To Create Contact");
+                Console.WriteLine("Select 2 : To Exit Creating Contact");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        addBook.CreateContact();
+                        addBook.Display();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+
         }
     }
 }
